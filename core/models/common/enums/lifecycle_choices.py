683c64c34b75f69aa658d975e512142394c4dc2e
Dalton Neely <daltonjn1@icloud.com>
2025-02-25 01:27:10 -0700

    Refactor

--- core/models/common/enums/lifecycle_choices.py
+++ core/models/common/enums/lifecycle_choices.py
@@ -1,15 +1,16 @@
-LIFECYCLE_ACTIVE = "Active"
-LIFECYCLE_APPROVAL = "Awaiting Approval"
-LIFECYCLE_DEPRECATED = "Deprecated"
-LIFECYCLE_DEVELOPMENT = "In Development"
-LIFECYCLE_HYPER_CARE = "Hyper Care"
-LIFECYCLE_IDEA = "Idea Submission"
-LIFECYCLE_IN_DEPRECATION = "In Deprecation Period"
-LIFECYCLE_LIMITED_SUPPORT = "Limited Support"
-LIFECYCLE_PLANNING = "In Planning"
-LIFECYCLE_REJECTED = "Approval Rejected"
+LIFECYCLE_ACTIVE: str = "Active"
+LIFECYCLE_APPROVAL: str = "Awaiting Approval"
+LIFECYCLE_DEPRECATED: str = "Deprecated"
+LIFECYCLE_DEVELOPMENT: str = "In Development"
+LIFECYCLE_HYPER_CARE: str = "Hyper Care"
+LIFECYCLE_IDEA: str = "Idea Submission"
+LIFECYCLE_IN_DEPRECATION: str = "In Deprecation Period"
+LIFECYCLE_LIMITED_SUPPORT: str = "Limited Support"
+LIFECYCLE_PLANNING: str = "In Planning"
+LIFECYCLE_REJECTED: str = "Approval Rejected"
 
-LIFECYCLE_CHOICES = [
+# noinspection DuplicatedCode
+LIFECYCLE_CHOICES: list[tuple[str, str]] = [
     (LIFECYCLE_ACTIVE, LIFECYCLE_ACTIVE),
     (LIFECYCLE_APPROVAL, LIFECYCLE_APPROVAL),
     (LIFECYCLE_DEPRECATED, LIFECYCLE_DEPRECATED),
