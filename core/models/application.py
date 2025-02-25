f4f5ef68532298a78bae7a03eaa48750797c777f
Dalton Neely <daltonjn1@icloud.com>
2025-02-25 12:44:59 -0700

    Add name_aliases field to Application model
    Introduced a new `name_aliases` field to the `Application` model, allowing storage of alternative names with a max length of 255. This change includes a corresponding database migration to support the new field. The field is optional, with support for null and blank values.

--- core/models/application.py
+++ core/models/application.py
@@ -26,6 +26,7 @@ class Application(Comment, Name):
     link_production_server = create_generic_varchar()
     link_production_server_external = create_generic_varchar()
     link_staging_server = create_generic_varchar()
+    name_aliases = create_generic_varchar()
     peak_userbase = create_generic_integer()
     person_architect = create_generic_fk(related_name='applications_as_architect', to=Person)
     person_developers = create_generic_m2m(related_name='applications_developer_of', to=Person)
