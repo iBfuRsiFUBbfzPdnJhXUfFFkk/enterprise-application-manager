fcf7db282a642f4befb01d04f5eb43d3d0c70b3c
Dalton Neely <daltonjn1@icloud.com>
2025-02-25 15:23:15 -0700

    Add new fields to Database model for improved configuration
    This commit introduces `hostname`, `ip_v4_internal`, `port`, and `schema` fields to the `Database` model, along with corresponding migrations. These changes enhance the model's capability to store essential database connection details for better flexibility and configuration.

--- core/models/database.py
+++ core/models/database.py
@@ -6,6 +6,7 @@ from core.models.common.enums.database_flavor_choices import DATABASE_FLAVOR_CHO
 from core.models.common.enums.environment_choices import ENVIRONMENT_CHOICES
 from core.models.common.field_factories.create_generic_enum import create_generic_enum
 from core.models.common.field_factories.create_generic_fk import create_generic_fk
+from core.models.common.field_factories.create_generic_integer import create_generic_integer
 from core.models.common.field_factories.create_generic_varchar import create_generic_varchar
 from core.utilities.encryption import encrypt_secret, decrypt_secret
 
@@ -14,6 +15,10 @@ class Database(Comment, Version):
     application = create_generic_fk(related_name='databases', to=Application)
     encrypted_password = create_generic_varchar()
     encrypted_username = create_generic_varchar()
+    hostname = create_generic_varchar()
+    ip_v4_internal = create_generic_varchar()
+    port = create_generic_integer()
+    schema = create_generic_varchar()
     type_data_storage_form = create_generic_enum(choices=DATA_STORAGE_FORM_CHOICES)
     type_database_flavor = create_generic_enum(choices=DATABASE_FLAVOR_CHOICES)
     type_environment = create_generic_enum(choices=ENVIRONMENT_CHOICES)
