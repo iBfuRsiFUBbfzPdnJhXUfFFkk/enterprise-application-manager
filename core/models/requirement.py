a8cc44583e707635f01c1437211ff1f54ec339fe
Dalton Neely <daltonjn1@icloud.com>
2025-02-26 17:20:31 -0700

    Add fields and relationships for SPSRD model and requirements
    This commit introduces new fields and ManyToMany relationships to the `ServiceProviderSecurityRequirementsDocument` and `Requirement` models. Fields like `is_for_spsrd` and `is_functional_requirement` were added to `Requirement`, while `applications`, `clients`, and `requirements` relationships were added to SPSRD. These additions enhance the flexibility of modeling and associating data.

--- core/models/requirement.py
+++ core/models/requirement.py
@@ -1,8 +1,12 @@
 from core.models.common.abstract.comment import Comment
 from core.models.common.abstract.name import Name
+from core.models.common.field_factories.create_generic_boolean import create_generic_boolean
 
 
 class Requirement(Comment, Name):
+    is_for_spsrd = create_generic_boolean()
+    is_functional_requirement = create_generic_boolean()
+
     def __str__(self):
         return f"{self.name}"
 
