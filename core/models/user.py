7bfd70a7abd6c5531ec513f90b114d2ee789c279
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-02-28 15:48:11 -0700

    Rename LDAP field and add AD attributes to user models
    Renamed `ldap_field` to `ldap_distinguished_name_dn` for clarity. Added new Active Directory (AD)-related fields and employee metadata to `User` and `HistoricalUser` models to support extended organizational attributes. Includes appropriate migration changes for database alignment.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- core/models/user.py
+++ core/models/user.py
@@ -6,5 +6,14 @@ from core.models.common.field_factories.create_generic_varchar import create_gen
 
 
 class User(AbstractUser, Comment):
+    active_directory_globally_unique_identifier_guid = create_generic_varchar()
+    active_directory_security_accounts_manager_account_name = create_generic_varchar()
+    active_directory_security_accounts_manager_account_type = create_generic_varchar()
+    active_directory_security_identifier_sid = create_generic_varchar()
+    employee_company = create_generic_varchar()
+    employee_department = create_generic_varchar()
+    employee_number = create_generic_varchar()
+    employee_telephone = create_generic_varchar()
+    employee_title = create_generic_varchar()
     history = HistoricalRecords(excluded_fields=['history_user'])
-    ldap_field = create_generic_varchar()
+    ldap_distinguished_name_dn = create_generic_varchar()
