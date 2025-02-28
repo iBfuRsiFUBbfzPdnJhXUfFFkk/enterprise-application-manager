383d8e27aa789c08c271feb6fa99b139f3e1d410
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-02-28 16:44:41 -0700

    Remove all `history_user` fields from models
    This commit removes the `history_user` field from multiple models and the `BaseModel` abstraction. It simplifies the historical tracking setup and eliminates the dependency on the `AUTH_USER_MODEL` for historical records.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- core/models/common/abstract/base_model.py
+++ core/models/common/abstract/base_model.py
@@ -1,17 +1,9 @@
-from django.db.models import Model, ForeignKey, SET_NULL
+from django.db.models import Model
 from simple_history.models import HistoricalRecords
-
-from core.settings import AUTH_USER_MODEL
 
 
 class BaseModel(Model):
     history = HistoricalRecords(inherit=True)
-    history_user = ForeignKey(
-        blank=True,
-        null=True,
-        on_delete=SET_NULL,
-        to=AUTH_USER_MODEL,
-    )
 
     class Meta:
         abstract = True
