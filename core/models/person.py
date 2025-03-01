65587a9a37921fce1a4350355b6820bba5ae708a
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-02-28 17:59:53 -0700

    Refactor GitLab sync fields and enhance user synchronization
    Renamed `link_gitlab_username` to `gitlab_sync_username` and added new fields for comprehensive GitLab data sync. Updated models, migrations, and templates to support these changes and implemented enhanced logic for syncing GitLab user details efficiently.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- core/models/person.py
+++ core/models/person.py
@@ -7,8 +7,10 @@ from core.models.common.enums.job_title_choices import JOB_TITLE_CHOICES
 from core.models.common.enums.timezone_choices import TIMEZONE_CHOICES
 from core.models.common.field_factories.create_generic_boolean import create_generic_boolean
 from core.models.common.field_factories.create_generic_date import create_generic_date
+from core.models.common.field_factories.create_generic_datetime import create_generic_datetime
 from core.models.common.field_factories.create_generic_enum import create_generic_enum
 from core.models.common.field_factories.create_generic_fk import create_generic_fk
+from core.models.common.field_factories.create_generic_integer import create_generic_integer
 from core.models.common.field_factories.create_generic_m2m import create_generic_m2m
 from core.models.common.field_factories.create_generic_varchar import create_generic_varchar
 from core.models.job_level import JobLevel
@@ -20,6 +22,17 @@ class Person(BaseModel, Comment, Location, Pronunciation):
     date_birthday = create_generic_date()
     date_hired = create_generic_date()
     date_left = create_generic_date()
+    gitlab_sync_access_level = create_generic_varchar()
+    gitlab_sync_avatar_url = create_generic_varchar()
+    gitlab_sync_datetime_created_at = create_generic_datetime()
+    gitlab_sync_datetime_expires_at = create_generic_datetime()
+    gitlab_sync_id = create_generic_varchar()
+    gitlab_sync_is_locked = create_generic_boolean()
+    gitlab_sync_membership_state = create_generic_varchar()
+    gitlab_sync_name = create_generic_varchar()
+    gitlab_sync_state = create_generic_varchar()
+    gitlab_sync_username = create_generic_varchar()
+    gitlab_sync_web_url = create_generic_varchar()
     is_active = create_generic_boolean(default=True)
     is_architect = create_generic_boolean()
     is_developer = create_generic_boolean()
@@ -30,7 +43,6 @@ class Person(BaseModel, Comment, Location, Pronunciation):
     is_project_manager = create_generic_boolean()
     is_scrum_master = create_generic_boolean()
     is_stakeholder = create_generic_boolean()
-    link_gitlab_username = create_generic_varchar()
     link_sharepoint_profile = create_generic_varchar()
     job_level = create_generic_fk(related_name='people_who_hold_this_job_level', to=JobLevel)
     name_first = create_generic_varchar()
--- core/views/this_api/this_api_sync_gitlab_users_view.py
+++ core/views/this_api/this_api_sync_gitlab_users_view.py
@@ -1,12 +1,36 @@
+from datetime import datetime, UTC
 from time import time
+from typing import TypedDict
 
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from requests import get
+from requests import get, Response
 
+from core.models.person import Person
 from core.models.secret import Secret
 from core.models.this_server_configuration import ThisServerConfiguration
 from core.views.generic.generic_500 import generic_500
+
+
+class GitlabApiResponseMember(TypedDict):
+    access_level: int
+    avatar_url: str
+    created_at: str
+    expires_at: str
+    id: int
+    locked: bool
+    membership_state: str
+    name: str
+    state: str
+    username: str
+    web_url: str
+
+
+def parse_datetime(datetime_str: str | None) -> datetime | None:
+    if datetime_str is None:
+        return None
+    dt: datetime = datetime.strptime(datetime_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+    return dt.replace(tzinfo=UTC)
 
 
 def this_api_sync_gitlab_users_view(request: HttpRequest) -> HttpResponse:
@@ -32,9 +56,52 @@ def this_api_sync_gitlab_users_view(request: HttpRequest) -> HttpResponse:
         return generic_500(request=request)
     headers: dict[str, str] = {"PRIVATE-TOKEN": decrypted_token}
     url: str = f"https://{connection_gitlab_hostname}/api/{connection_gitlab_api_version}/groups/{connection_gitlab_group_id}/members"
-    response = get(headers=headers, url=url)
-    response.raise_for_status()
-    print(response.json())
+    gitlab_user_objects: list[GitlabApiResponseMember] = []
+    while url is not None:
+        response: Response = get(headers=headers, url=url)
+        response.raise_for_status()
+        gitlab_user_objects.extend(response.json())
+        url: str | None = response.links.get("next", {}).get("url")
+    for gitlab_user_object in gitlab_user_objects:
+        gitlab_access_level_int: int | None = gitlab_user_object.get(key="access_level", default=None)
+        gitlab_id_int: int | None = gitlab_user_object.get(key="id", default=None)
+        gitlab_name: str | None = gitlab_user_object.get(key="name", default=None)
+        gitlab_names: list[str] = (gitlab_name or "").split(" ")
+        gitlab_username: str | None = gitlab_user_object.get(key="username", default=None)
+        gitlab_first_name: str = gitlab_names[0] or ""
+        gitlab_last_name: str = gitlab_names[1] or ""
+        person: Person | None = Person.objects.filter(
+            gitlab_sync_id=str(object=gitlab_id_int),
+            gitlab_sync_id__isnull=False,
+        ).first()
+        if person is None:
+            person: Person | None = Person.objects.filter(
+                gitlab_sync_username=gitlab_username,
+                gitlab_sync_username__isnull=False,
+            ).first()
+            if person is None:
+                person: Person | None = Person.objects.filter(
+                    name_first=gitlab_first_name,
+                    name_first__isnull=False,
+                    name_last=gitlab_last_name,
+                    name_last__isnull=False,
+                ).first()
+                if person is None:
+                    person: Person = Person.objects.create()
+        person.gitlab_sync_access_level = str(
+            object=gitlab_access_level_int) if gitlab_access_level_int is not None else None
+        person.gitlab_sync_avatar_url = gitlab_user_object.get(key="avatar_url", default=None)
+        person.gitlab_sync_datetime_created_at = parse_datetime(
+            datetime_str=gitlab_user_object.get(key="created_at", default=None))
+        person.gitlab_sync_datetime_expires_at = parse_datetime(
+            datetime_str=gitlab_user_object.get(key="expires_at", default=None))
+        person.gitlab_sync_id = str(object=gitlab_id_int) if gitlab_id_int is not None else None
+        person.gitlab_sync_is_locked = gitlab_user_object.get(key="locked", default=False)
+        person.gitlab_sync_membership_state = gitlab_user_object.get(key="membership_state", default=None)
+        person.gitlab_sync_name = gitlab_name
+        person.gitlab_sync_state = gitlab_user_object.get(key="state", default=None)
+        person.gitlab_sync_username = gitlab_username
+        person.gitlab_sync_web_url = gitlab_user_object.get(key="web_url", default=None)
     end_time: float = time()
     execution_time: float = end_time - start_time
     print(f'Execution time: {execution_time} seconds')
