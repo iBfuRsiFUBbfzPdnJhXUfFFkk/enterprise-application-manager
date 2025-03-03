0e08a1b21e35e3904fc482cc82d7817499168716
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-03 12:30:42 -0700

    Set a timeout for HTTP requests in action_run_view.
    Added a 180-second timeout to the `get` request in `action_run_view` to ensure the server doesn't hang indefinitely while waiting for a response. This improves the robustness of the method and prevents potential performance bottlenecks.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- core/views/action/action_run_view.py
+++ core/views/action/action_run_view.py
@@ -11,13 +11,16 @@ from core.utilities.base_render import base_render
 def action_run_view(request: HttpRequest, model_id: int) -> HttpResponse:
     start_time: float = time()
     action: Action = get_object_or_404(Action, id=model_id)
-    response: Response = get(url=action.url)
+    response: Response = get(timeout=180, url=action.url)
     response.raise_for_status()
     end_time: float = time()
     execution_time_in_seconds: float = end_time - start_time
     print(f'Execution time: {execution_time_in_seconds} seconds')
     return base_render(
-        context={"execution_time_in_seconds": execution_time_in_seconds},
+        context={
+            "action_url": action.url,
+            "execution_time_in_seconds": execution_time_in_seconds,
+        },
         request=request,
         template_name="authenticated/action/action_success.html"
     )
