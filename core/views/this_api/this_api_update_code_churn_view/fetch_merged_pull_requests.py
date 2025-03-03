e4c12b87f16b823b8242c7df9f77b96335bf6ad5
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-03 11:05:43 -0700

    Add TypedDict definitions and enhance fetch logic
    Introduce `TypedDict` classes for better type safety and readability when working with merge request data. Refactor `fetch_merged_pull_requests` to include type hints, handle missing parameters more robustly, and log specific fields as JSON.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- core/views/this_api/this_api_update_code_churn_view/fetch_merged_pull_requests.py
+++ core/views/this_api/this_api_update_code_churn_view/fetch_merged_pull_requests.py
@@ -1,6 +1,9 @@
+from json import dumps
 from typing import Any, Mapping
 
 from requests import get, Response
+
+from core.views.this_api.this_api_update_code_churn_view.update_code_churn_typed_dicts import MergeRequest
 
 
 def fetch_merged_pull_requests(
@@ -10,7 +13,16 @@ def fetch_merged_pull_requests(
         decrypted_token: str | None = None,
         merged_after: str | None = None,
         merged_before: str | None = None,
-):
+) -> list[MergeRequest] | None:
+    if (
+            connection_gitlab_hostname is None
+            or connection_gitlab_api_version is None
+            or connection_gitlab_group_id is None
+            or decrypted_token is None
+            or merged_after is None
+            or merged_before is None
+    ):
+        return None
     url: str = f"https://{connection_gitlab_hostname}/api/{connection_gitlab_api_version}/groups/{connection_gitlab_group_id}/merge_requests"
     parameters: dict[str, Any] = {
         "state": "merged",
@@ -18,7 +30,7 @@ def fetch_merged_pull_requests(
         "updated_before": merged_before,
         "per_page": 100,
     }
-    all_pull_requests = []
+    all_pull_requests: list[MergeRequest] = []
     headers: Mapping[str, str] = {"PRIVATE-TOKEN": decrypted_token}
     while url is not None:
         response: Response = get(
@@ -27,7 +39,8 @@ def fetch_merged_pull_requests(
             url=url,
         )
         response.raise_for_status()
+        pull_request: MergeRequest = response.json()
         all_pull_requests.extend(response.json())
-        print(response.json())
+        print(dumps({"iid": pull_request["iid"], "username": pull_request["author"]["username"]}))
         url: str | None = response.links.get("next", {}).get("url")
     return all_pull_requests
