282f42e826193f8f0b2ae09bd5468194748ae08b
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-04 14:17:43 -0700

    Add API endpoint to update sprint delivered issues
    This commit introduces a new API endpoint, `this_api_update_sprint_delivered_issues`, to calculate and update the number of delivered story points, context switches, and corresponding KPIs for developers across sprints. It includes the logic to fetch, process, and store the data into the database, ensuring accurate metrics tracking.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- core/urls/urlpatterns_this_api.py
+++ core/urls/urlpatterns_this_api.py
@@ -10,8 +10,11 @@ from core.views.this_api.this_api_update_issues_authored_view.this_api_update_is
     this_api_update_issues_authored_view
 from core.views.this_api.this_api_update_sprint_committed_issues_view.this_api_update_sprint_committed_issues_view import \
     this_api_update_sprint_committed_issues_view
+from core.views.this_api.this_api_update_sprint_delivered_issues_view.this_api_update_sprint_delivered_issues_view import \
+    this_api_update_sprint_delivered_issues_view
 
 urlpatterns_this_api: list[URLPattern | URLResolver] = [
+    path(name="this_api_update_sprint_delivered_issues", route="api/update_sprint_delivered_issues/", view=this_api_update_sprint_delivered_issues_view),
     path(name="this_api_create_sprint_kpis", route="api/create_sprint_kpis/", view=this_api_create_sprint_kpis_view),
     path(name="this_api_sync_gitlab_users", route="api/sync_gitlab_users/", view=this_api_sync_gitlab_users_view),
     path(name="this_api_update_code_churn", route="api/update_code_churn/", view=this_api_update_code_churn_view),
