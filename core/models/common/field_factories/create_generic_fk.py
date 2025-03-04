b007891ad77f954dacbdb250f7e959f6fc7e2ee0
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-04 14:01:28 -0700

    Update ForeignKey factory to allow string type for `to` argument
    Modified `create_generic_fk` to accept strings as a valid `to` type, enabling more flexibility in referencing models. Updated `GitLabIteration` to use string-based model reference for the `sprint` field.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- core/models/common/field_factories/create_generic_fk.py
+++ core/models/common/field_factories/create_generic_fk.py
@@ -3,7 +3,7 @@ from django.db.models import ForeignKey, Model, DO_NOTHING
 
 def create_generic_fk(
         related_name: str | None = None,
-        to: type[Model] | None = None,
+        to: type[Model] | str | None = None,
 ) -> ForeignKey:
     return ForeignKey(
         blank=True,
--- core/models/git_lab_iteration.py
+++ core/models/git_lab_iteration.py
@@ -3,12 +3,11 @@ from core.models.common.abstract.comment import Comment
 from core.models.common.abstract.name import Name
 from core.models.common.field_factories.create_generic_fk import create_generic_fk
 from core.models.common.field_factories.create_generic_varchar import create_generic_varchar
-from core.models.sprint import Sprint
 
 
 class GitLabIteration(BaseModel, Comment, Name):
     git_lab_id = create_generic_varchar()
-    sprint = create_generic_fk(related_name="git_lab_iterations", to=Sprint)
+    sprint = create_generic_fk(related_name="git_lab_iterations", to='Sprint')
 
     def __str__(self):
         return str(self.id)
