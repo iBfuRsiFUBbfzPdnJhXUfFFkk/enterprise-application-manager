eb556e7bc70a2e080407acdb5dfadf21c4aabc93
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-04 17:09:24 -0700

    Update merge request approvals handling to use approval state
    Replaced `ProjectMergeRequestApproval` with `ProjectMergeRequestApprovalState` to align with updated GitLab API changes. Adjusted method to fetch approval states using `.approval_state.list()` for consistency and accuracy.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- core/views/this_api/this_api_sync_git_lab_view/common/fetch_project_merge_requests_approvals.py
+++ core/views/this_api/this_api_sync_git_lab_view/common/fetch_project_merge_requests_approvals.py
@@ -1,7 +1,7 @@
 from typing import cast
 
 from gitlab import Gitlab
-from gitlab.v4.objects import ProjectMergeRequestApproval
+from gitlab.v4.objects import ProjectMergeRequestApprovalState
 
 from core.utilities.git_lab.get_git_lab_client import get_git_lab_client
 
@@ -9,17 +9,17 @@ from core.utilities.git_lab.get_git_lab_client import get_git_lab_client
 def fetch_project_merge_requests_approvals(
         merge_request_internal_identification_iid: int | str | None = None,
         project_id: int | str | None = None,
-) -> list[ProjectMergeRequestApproval] | None:
+) -> list[ProjectMergeRequestApprovalState] | None:
     if merge_request_internal_identification_iid is None or project_id is None:
         return None
     git_lab_client: Gitlab | None = get_git_lab_client()
     if git_lab_client is None:
         return None
     return cast(
-        typ=list[ProjectMergeRequestApproval],
+        typ=list[ProjectMergeRequestApprovalState],
         val=(
             git_lab_client.projects.get(project_id)
             .mergerequests.get(merge_request_internal_identification_iid)
-            .approvals
+            .approval_state.list()
         )
     )
