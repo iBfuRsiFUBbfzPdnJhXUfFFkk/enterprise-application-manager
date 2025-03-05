363a5bb9c6cb76a9299e79c8de004f05cf59a7f6
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-05 12:36:32 -0700

    Add tracking for action executions and user interactions
    This commit introduces fields to track the datetime, user, and count of action executions, and updates the Action model, migrations, and templates accordingly. Additionally, the action execution view now updates these fields and calculates the estimated runtime dynamically. These changes improve visibility and auditability of action usage.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- core/models/action.py
+++ core/models/action.py
@@ -2,11 +2,17 @@ from core.models.common.abstract.base_model import BaseModel
 from core.models.common.abstract.comment import Comment
 from core.models.common.abstract.name import Name
 from core.models.common.abstract.url import UniformResourceLocator
+from core.models.common.field_factories.create_generic_datetime import create_generic_datetime
+from core.models.common.field_factories.create_generic_fk import create_generic_fk
 from core.models.common.field_factories.create_generic_integer import create_generic_integer
+from core.models.user import User
 
 
 class Action(BaseModel, Comment, Name, UniformResourceLocator):
+    datetime_of_last_run = create_generic_datetime()
     estimated_run_time_in_seconds = create_generic_integer()
+    number_of_times_run = create_generic_integer()
+    user_of_last_run = create_generic_fk(to=User)
 
     def __str__(self):
         return f"{self.name}"
--- core/views/action/action_run_view.py
+++ core/views/action/action_run_view.py
@@ -1,21 +1,33 @@
+from datetime import datetime, UTC
 from time import time
 
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AnonymousUser
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404
 from requests import get, Response
 
 from core.models.action import Action
+from core.models.user import User
 from core.utilities.base_render import base_render
 
 
 def action_run_view(request: HttpRequest, model_id: int) -> HttpResponse:
     start_time: float = time()
+    user: AbstractBaseUser | AnonymousUser = request.user
+    user_id: int = user.id
+    user_instance: User | None = User.objects.filter(id=user_id).first()
     action: Action = get_object_or_404(Action, id=model_id)
     response: Response = get(cookies=request.COOKIES, timeout=180, url=action.url)
     response.raise_for_status()
+    action.number_of_times_run = (action.number_of_times_run or 0) + 1
+    action.user_of_last_run = user_instance
+    action.datetime_of_last_run = datetime.now(tz=UTC)
+    action.save()
     end_time: float = time()
     execution_time_in_seconds: float = end_time - start_time
-    print(f'Execution time: {execution_time_in_seconds} seconds')
+    action.estimated_run_time_in_seconds = int(execution_time_in_seconds)
+    action.save()
     return base_render(
         context={
             "action_url": action.url,
