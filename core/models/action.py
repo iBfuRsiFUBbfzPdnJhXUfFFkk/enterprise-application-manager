62b2044f90e408ab1b08a93bf0dc36d3b1d1b89e
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-05 12:15:31 -0700

    Add estimated_run_time_in_seconds field to Action models
    This commit introduces a new field, `estimated_run_time_in_seconds`, to both the `Action` and `HistoricalAction` models. The field is nullable and utilizes a generic integer factory for consistency. A corresponding database migration is also included.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- core/models/action.py
+++ core/models/action.py
@@ -2,9 +2,12 @@ from core.models.common.abstract.base_model import BaseModel
 from core.models.common.abstract.comment import Comment
 from core.models.common.abstract.name import Name
 from core.models.common.abstract.url import UniformResourceLocator
+from core.models.common.field_factories.create_generic_integer import create_generic_integer
 
 
 class Action(BaseModel, Comment, Name, UniformResourceLocator):
+    estimated_run_time_in_seconds = create_generic_integer()
+
     def __str__(self):
         return f"{self.name}"
 
