87a3438978a057c2374f7b06fc0b9d1e4d42537f
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-05 18:11:10 -0700

    Add unique and non-editable UUIDs to models
    Updated `enumeration_attack_uuid` fields across multiple models to be unique and non-editable by default. Updated the field factory to reflect these changes, ensuring consistency throughout the codebase. Removed unused code in the `BaseModelAdmin` class.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- core/admin/base_model_admin.py
+++ core/admin/base_model_admin.py
@@ -3,5 +3,3 @@ from simple_history.admin import SimpleHistoryAdmin
 
 class BaseModelAdmin(SimpleHistoryAdmin):
     pass
-    # noinspection PyUnresolvedReferences
-    # exclude = ['enumeration_attack_uuid']
--- core/models/common/field_factories/create_generic_uuid.py
+++ core/models/common/field_factories/create_generic_uuid.py
@@ -4,4 +4,4 @@ from django.db.models.fields import UUIDField
 
 
 def create_generic_uuid() -> UUIDField:
-    return UUIDField(blank=True, default=uuid4, null=True)
+    return UUIDField(blank=True, default=uuid4, editable=False, unique=True)
