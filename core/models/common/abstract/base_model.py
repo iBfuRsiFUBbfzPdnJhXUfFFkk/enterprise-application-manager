2be8ff6386bd52a664579f18208d319841dbfa11
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-07 12:07:44 -0700

    Refactor historical models to improve consistency.
    Updated historical model options to ensure consistent ordering and latest selection logic. Refactored `BaseModel` to dynamically set `HistoricalRecords` with class-specific verbose names and pluralizations.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- core/models/common/abstract/base_model.py
+++ core/models/common/abstract/base_model.py
@@ -6,7 +6,16 @@ from core.models.common.field_factories.create_generic_uuid import create_generi
 
 class BaseModel(Model):
     enumeration_attack_uuid: str | None = create_generic_uuid()
-    history = HistoricalRecords(inherit=True)
+    history: HistoricalRecords = None
 
     class Meta:
         abstract = True
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        cls.history = HistoricalRecords(
+            inherit=True,
+            verbose_name=f"_Historical Record for {cls.__name__}",
+            verbose_name_plural=f"_Historical Records for {cls.__name__}",
+        )
+        cls.add_to_class(name='history', value=cls.history)
