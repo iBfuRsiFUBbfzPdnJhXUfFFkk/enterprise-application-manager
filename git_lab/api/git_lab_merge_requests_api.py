15b26b5d51435e52d250b1e74d525ee6b533d2bf
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-08 15:00:34 -0700

    Add API endpoint for GitLab merge requests
    Introduced a new API endpoint to retrieve and aggregate GitLab merge requests across groups. This includes implementing the `git_lab_merge_requests_api` view and integrating it into the URL configuration. Ensures proper error handling and data serialization for response.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

---
+++ git_lab/api/git_lab_merge_requests_api.py
@@ -0,0 +1,34 @@
+from typing import cast
+
+from django.db.models import QuerySet
+from django.http import HttpRequest, JsonResponse, HttpResponse
+from gitlab import Gitlab
+from gitlab.v4.objects import Group, GroupMergeRequest
+
+from core.utilities.cast_query_set import cast_query_set
+from core.utilities.git_lab.get_git_lab_client import get_git_lab_client
+from core.views.generic.generic_500 import generic_500
+from git_lab.models.git_lab_group import GitLabGroup
+
+
+def git_lab_merge_requests_api(
+        request: HttpRequest,
+) -> JsonResponse | HttpResponse:
+    git_lab_client: Gitlab | None = get_git_lab_client()
+    if git_lab_client is None:
+        return generic_500(request=request)
+    git_lab_groups: QuerySet[GitLabGroup] = cast_query_set(
+        typ=GitLabGroup,
+        val=GitLabGroup.objects.all(),
+    )
+    all_merge_requests: set[GroupMergeRequest] = set()
+    for git_lab_group in git_lab_groups:
+        group: Group | None = git_lab_client.groups.get(id=git_lab_group.id)
+        if group is None:
+            continue
+        merge_requests: list[GroupMergeRequest] = cast(typ=list[GroupMergeRequest],
+                                                       val=group.mergerequests.list(all=True))
+        for merge_request in merge_requests:
+            all_merge_requests.add(merge_request)
+    merge_request_dicts: list[dict] = [project.asdict() for project in list(all_merge_requests)]
+    return JsonResponse(data=merge_request_dicts, safe=False)
--- git_lab/urls/__init__.py
+++ git_lab/urls/__init__.py
@@ -1,5 +1,6 @@
 from django.urls import path, URLPattern, URLResolver
 
+from git_lab.api.git_lab_merge_requests_api import git_lab_merge_requests_api
 from git_lab.api.git_lab_users_api import git_lab_users_api
 from git_lab.api.git_lab_groups_api import git_lab_groups_api
 from git_lab.api.git_lab_projects_api import git_lab_projects_api
@@ -8,6 +9,7 @@ app_name: str = 'git_lab'
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path(name='git_lab_groups', route='group/', view=git_lab_groups_api),
+    path(name='git_lab_merge_requests', route='merge_request/', view=git_lab_merge_requests_api),
     path(name='git_lab_projects', route='project/', view=git_lab_projects_api),
     path(name='git_lab_users', route='user/', view=git_lab_users_api),
 ]
