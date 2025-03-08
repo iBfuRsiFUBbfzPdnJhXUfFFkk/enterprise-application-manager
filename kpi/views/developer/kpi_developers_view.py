ad450296f522d05d3680753744021b5d8aa4f4c4
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-07 20:18:05 -0700

    Update developers list to show only actively employed
    Replaced the `Person.developers()` call with `Person.developers_actively_employed()` to filter developers and include only those currently employed. This ensures more accurate data is displayed in the KPIs.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- kpi/views/developer/kpi_developers_view.py
+++ kpi/views/developer/kpi_developers_view.py
@@ -7,7 +7,7 @@ from core.utilities.base_render import base_render
 def kpi_developers_view(request: HttpRequest) -> HttpResponse:
     return base_render(
         context={
-            "developers": Person.developers(),
+            "developers": Person.developers_actively_employed(),
         },
         request=request,
         template_name="authenticated/kpi/developer/kpi_developers.html"
