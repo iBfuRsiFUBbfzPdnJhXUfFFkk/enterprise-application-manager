e2a51dc9887ad9bffb3f37cba4fec72ca6fe520b
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-08 13:55:43 -0700

    Add GitLab projects API endpoint
    Introduce a new API endpoint that retrieves projects from all GitLab groups and returns them as JSON. Updated the URL configuration to include the new `project/` endpoint under the `git_lab` app.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- git_lab/urls/__init__.py
+++ git_lab/urls/__init__.py
@@ -2,10 +2,12 @@ from django.urls import path, URLPattern, URLResolver
 
 from git_lab.api.git_lab_group_members_api import git_lab_group_members_api
 from git_lab.api.git_lab_groups_api import git_lab_groups_api
+from git_lab.api.git_lab_projects_api import git_lab_projects_api
 
 app_name: str = 'git_lab'
 
 urlpatterns: list[URLPattern | URLResolver] = [
     path(name='git_lab_groups', route='group/', view=git_lab_groups_api),
+    path(name='git_lab_projects', route='project/', view=git_lab_projects_api),
     path(name='git_lab_group_members', route='group/member/', view=git_lab_group_members_api),
 ]
