3d100e426f61157281c721c5081f037b38de2999
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-08 20:49:50 -0700

    Add properties to fetch user-related merge requests
    Introduced properties to retrieve merge requests assigned, authored, closed, merged, and reviewed by a user. These methods utilize the `cast_query_set` utility for type-safe query handling. Improves ease of access to user-related merge request data.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- git_lab/models/git_lab_user.py
+++ git_lab/models/git_lab_user.py
@@ -5,6 +5,7 @@ from core.models.common.abstract.abstract_name import AbstractName
 from core.models.common.field_factories.create_generic_boolean import create_generic_boolean
 from core.models.common.field_factories.create_generic_datetime import create_generic_datetime
 from core.models.common.field_factories.create_generic_varchar import create_generic_varchar
+from core.utilities.cast_query_set import cast_query_set
 from git_lab.models.common.abstract.abstract_git_lab_avatar_url import AbstractGitLabAvatarUrl
 from git_lab.models.common.abstract.abstract_git_lab_created_at import AbstractGitLabCreatedAt
 from git_lab.models.common.abstract.abstract_git_lab_primary_key import AbstractGitLabPrimaryKey
@@ -24,6 +25,46 @@ class GitLabUser(
     expires_at: datetime | None = create_generic_datetime()
     locked: bool | None = create_generic_boolean()
     username: str | None = create_generic_varchar()
+
+    @property
+    def merge_requests_assigned(self):
+        from git_lab.models.git_lab_merge_request import GitLabMergeRequest
+        return cast_query_set(
+            typ=GitLabMergeRequest,
+            val=GitLabMergeRequest.objects.filter(assignees__in=[self])
+        )
+
+    @property
+    def merge_requests_authored(self):
+        from git_lab.models.git_lab_merge_request import GitLabMergeRequest
+        return cast_query_set(
+            typ=GitLabMergeRequest,
+            val=GitLabMergeRequest.objects.filter(author=self)
+        )
+
+    @property
+    def merge_requests_closed(self):
+        from git_lab.models.git_lab_merge_request import GitLabMergeRequest
+        return cast_query_set(
+            typ=GitLabMergeRequest,
+            val=GitLabMergeRequest.objects.filter(closed_by=self)
+        )
+
+    @property
+    def merge_requests_merged(self):
+        from git_lab.models.git_lab_merge_request import GitLabMergeRequest
+        return cast_query_set(
+            typ=GitLabMergeRequest,
+            val=GitLabMergeRequest.objects.filter(merged_by=self)
+        )
+
+    @property
+    def merge_requests_reviewed(self):
+        from git_lab.models.git_lab_merge_request import GitLabMergeRequest
+        return cast_query_set(
+            typ=GitLabMergeRequest,
+            val=GitLabMergeRequest.objects.filter(reviewers__in=[self])
+        )
 
     def __str__(self) -> str:
         return f"{self.username}"
