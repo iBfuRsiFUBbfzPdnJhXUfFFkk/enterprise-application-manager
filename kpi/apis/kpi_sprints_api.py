24960055dc3982d3732057cbf6af93574facc39c
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-11 14:39:44 -0600

    Add tracking of approved merge requests in KPI sprints
    This update introduces the tracking of merge requests approved by GitLab users within sprint dates. The `number_of_merge_requests_approved` field is now populated with the count, enhancing sprint performance metrics.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- kpi/apis/kpi_sprints_api.py
+++ kpi/apis/kpi_sprints_api.py
@@ -6,6 +6,7 @@ from django.http import HttpRequest, JsonResponse, HttpResponse
 from core.utilities.cast_query_set import cast_query_set
 from git_lab.models.git_lab_issue import GitLabIssue
 from git_lab.models.git_lab_iteration import GitLabIteration
+from git_lab.models.git_lab_merge_request import GitLabMergeRequest
 from git_lab.models.git_lab_user import GitLabUser
 from kpi.models.key_performance_indicator_sprint import KeyPerformanceIndicatorSprint
 from scrum.models.scrum_sprint import ScrumSprint
@@ -37,9 +38,14 @@ def kpi_sprints_api(
             issues_authored: QuerySet[GitLabIssue] = git_lab_user.issues_authored.filter(iteration__in=iterations)
             issues_assigned: QuerySet[GitLabIssue] = git_lab_user.issues_assigned.filter(iteration__in=iterations)
             issues_closed: QuerySet[GitLabIssue] = issues_assigned.filter(state="closed")
+            merge_requests: QuerySet[GitLabMergeRequest] = git_lab_user.merge_requests_reviewed.filter(
+                merged_at__date__gte=scrum_sprint.date_start,
+                merged_at__date__lte=scrum_sprint.date_end,
+            )
             kpi_sprint.name = f"{scrum_sprint.name} - {git_lab_user.username}"
             kpi_sprint.git_lab_iterations.add(*iterations)
             kpi_sprint.git_lab_issues.add(*issues_authored)
+            kpi_sprint.number_of_merge_requests_approved = merge_requests.count()
             kpi_sprint.number_of_issues_written = issues_authored.count()
             kpi_sprint.number_of_story_points_committed_to = sum([
                 issue.weight
