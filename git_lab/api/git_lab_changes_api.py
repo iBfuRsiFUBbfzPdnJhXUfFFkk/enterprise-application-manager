2a69c46f132fec76e5630c7f54bbf008c6bff249
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-11 14:58:46 -0600

    Refactor to fetch changes via GitLabProject instead of MergeRequest
    Replaced GitLabMergeRequest with GitLabProject to optimize data retrieval. The updated logic iterates over projects and fetches changes for their merge requests. This change improves efficiency by reducing the number of API calls.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- git_lab/api/git_lab_changes_api.py
+++ git_lab/api/git_lab_changes_api.py
@@ -9,6 +9,7 @@ from core.utilities.cast_query_set import cast_query_set
 from core.utilities.git_lab.get_git_lab_client import get_git_lab_client
 from core.views.generic.generic_500 import generic_500
 from git_lab.models.git_lab_merge_request import GitLabMergeRequest
+from git_lab.models.git_lab_project import GitLabProject
 
 
 def git_lab_changes_api(
@@ -18,19 +19,17 @@ def git_lab_changes_api(
     git_lab_client: Gitlab | None = get_git_lab_client()
     if git_lab_client is None:
         return generic_500(request=request)
-    git_lab_merge_requests: QuerySet[GitLabMergeRequest] = cast_query_set(
-        typ=GitLabMergeRequest,
-        val=GitLabMergeRequest.objects.all(),
+    git_lab_projects: QuerySet[GitLabProject] = cast_query_set(
+        typ=GitLabProject,
+        val=GitLabProject.objects.all()[::5],
     )
     all_merge_request_changes: set[dict[str, Any]] = set()
-    for git_lab_merge_request in git_lab_merge_requests:
-        merge_request: MergeRequest | None = git_lab_client.mergerequests.get(id=git_lab_merge_request.id)
-        if merge_request is None:
+    for git_lab_project in git_lab_projects:
+        changes: list[dict[str, Any]] | None = [x.changes for x in git_lab_client.projects.get(
+            id=git_lab_project.id, lazy=True
+        ).mergerequests.list(all=True, lazy=True)]
+        if changes is None:
             continue
-        changes: list[dict[str, Any]] = cast(
-            typ=list[dict[str, Any]],
-            val=merge_request.changes.list(page=1, per_page=fetch_per_group)
-        )
         for change in changes:
             all_merge_request_changes.add(change)
     # merge_request_dicts: list[GitLabMergeRequestTypedDict] = [project.asdict() for project in list(all_merge_request_changes)]
