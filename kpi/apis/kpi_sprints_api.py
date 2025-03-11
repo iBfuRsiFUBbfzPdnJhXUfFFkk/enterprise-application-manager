26789ca771887f6fd41a247c0799ba950a7cb2f5
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-11 10:48:14 -0600

    Update default fetch count and add debugging print statements
    Changed the default `fetch_per_group` value from 2 to 1 to align with updated requirements. Added print statements for debugging `scrum_sprint.name`, `git_lab_user.username`, `iterations`, and `issues` to assist in runtime inspection.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- kpi/apis/kpi_sprints_api.py
+++ kpi/apis/kpi_sprints_api.py
@@ -14,7 +14,7 @@ from scrum.models.scrum_sprint import ScrumSprint
 def kpi_sprints_api(
         request: HttpRequest,
 ) -> JsonResponse | HttpResponse:
-    fetch_per_group: int = request.GET.get('fetch_per_group', 2)
+    fetch_per_group: int = request.GET.get('fetch_per_group', 1)
     current_date = date.today()
     scrum_sprints: QuerySet[ScrumSprint] = cast_query_set(
         typ=ScrumSprint,
@@ -26,6 +26,7 @@ def kpi_sprints_api(
     )
     for scrum_sprint in scrum_sprints:
         for git_lab_user in git_lab_users:
+            print(f"{scrum_sprint.name} - {git_lab_user.username}")
             iterations: QuerySet[GitLabIteration] = scrum_sprint.iterations.all()
             kpi_sprint: KeyPerformanceIndicatorSprint = KeyPerformanceIndicatorSprint.objects.filter(
                 git_lab_user=git_lab_user,
@@ -36,6 +37,8 @@ def kpi_sprints_api(
             )
             kpi_sprint.name = f"{scrum_sprint.name} - {git_lab_user.username}"
             issues: QuerySet[GitLabIssue] = git_lab_user.issues_authored.filter(iteration__in=iterations)
+            print(iterations)
+            print(issues)
             kpi_sprint.number_of_issues_written = issues.count()
             kpi_sprint.save()
     return JsonResponse(data={}, safe=False)
