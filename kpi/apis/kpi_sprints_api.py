e5a0779756ece35451543558c45843d904f5bb3f
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-11 10:24:51 -0600

    Refactor KPI sprints API to include iterations and issues.
    Updated the KPI sprints logic to associate sprint iterations and calculate the number of issues authored by users per sprint. Adjusted query handling for better data segmentation using `fetch_per_group`.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- kpi/apis/kpi_sprints_api.py
+++ kpi/apis/kpi_sprints_api.py
@@ -2,6 +2,8 @@ from django.db.models import QuerySet
 from django.http import HttpRequest, JsonResponse, HttpResponse
 
 from core.utilities.cast_query_set import cast_query_set
+from git_lab.models.git_lab_issue import GitLabIssue
+from git_lab.models.git_lab_iteration import GitLabIteration
 from git_lab.models.git_lab_user import GitLabUser
 from kpi.models.key_performance_indicator_sprint import KeyPerformanceIndicatorSprint
 from scrum.models.scrum_sprint import ScrumSprint
@@ -10,10 +12,10 @@ from scrum.models.scrum_sprint import ScrumSprint
 def kpi_sprints_api(
         request: HttpRequest,
 ) -> JsonResponse | HttpResponse:
-    fetch_per_group: int = request.GET.get('fetch_per_group', 25)
+    fetch_per_group: int = request.GET.get('fetch_per_group', 2)
     scrum_sprints: QuerySet[ScrumSprint] = cast_query_set(
         typ=ScrumSprint,
-        val=ScrumSprint.objects.all()
+        val=ScrumSprint.objects.all()[::fetch_per_group]
     )
     git_lab_users: QuerySet[GitLabUser] = cast_query_set(
         typ=GitLabUser,
@@ -21,7 +23,7 @@ def kpi_sprints_api(
     )
     for scrum_sprint in scrum_sprints:
         for git_lab_user in git_lab_users:
-            print(f"{scrum_sprint.name} - {git_lab_user.name}")
+            iterations: QuerySet[GitLabIteration] = scrum_sprint.iterations
             kpi_sprint: KeyPerformanceIndicatorSprint = KeyPerformanceIndicatorSprint.objects.filter(
                 git_lab_user=git_lab_user,
                 scrum_sprint=scrum_sprint,
@@ -29,5 +31,8 @@ def kpi_sprints_api(
                 git_lab_user=git_lab_user,
                 scrum_sprint=scrum_sprint,
             )
+            kpi_sprint.name = f"{scrum_sprint.name} - {git_lab_user.name}"
+            issues: QuerySet[GitLabIssue] = git_lab_user.issues_authored.filter(iteration__in=iterations)
+            kpi_sprint.number_of_issues_written = issues.count()
             kpi_sprint.save()
     return JsonResponse(data={}, safe=False)
