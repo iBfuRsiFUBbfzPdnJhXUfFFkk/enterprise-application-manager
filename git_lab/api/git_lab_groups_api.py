8f13b3d2399e89637e109500f62deda9a2d7d401
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-12 20:46:18 -0600

    Refactor GitLab API to handle query parameters and errors
    Updated both user and group API functions to handle common query parameters and capture GitLabListError exceptions gracefully. Refactored redundant code for fetching or creating records, ensuring better clarity and error handling. This improves API reliability and flexibility in managing GitLab data.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- git_lab/api/git_lab_groups_api.py
+++ git_lab/api/git_lab_groups_api.py
@@ -8,6 +8,7 @@ from core.models.this_server_configuration import ThisServerConfiguration
 from core.utilities.convert_and_enforce_utc_timezone import convert_and_enforce_utc_timezone
 from core.utilities.git_lab.get_git_lab_client import get_git_lab_client
 from core.views.generic.generic_500 import generic_500
+from git_lab.api.common.get_common_query_parameters import GitLabApiCommonQueryParameters, get_common_query_parameters
 from git_lab.models.common.typed_dicts.git_lab_group_typed_dict import GitLabGroupTypedDict
 from git_lab.models.git_lab_group import GitLabGroup
 
@@ -16,6 +17,7 @@ def recurse_groups(
         all_groups: set[Group] | None = None,
         git_lab_client: Gitlab | None = None,
         parent_group: Group | None = None,
+        query_parameters: GitLabApiCommonQueryParameters | None = None,
 ) -> set[Group]:
     if all_groups is None:
         all_groups = set()
@@ -25,7 +27,7 @@ def recurse_groups(
         return all_groups
     subgroups: list[GroupSubgroup] = cast(
         typ=list[GroupSubgroup],
-        val=parent_group.subgroups.list()
+        val=parent_group.subgroups.list(**(query_parameters or dict(all=True)))
     )
     for subgroup in subgroups:
         child_group: Group | None = git_lab_client.groups.get(id=subgroup.id)
@@ -36,6 +38,7 @@ def recurse_groups(
             all_groups=all_groups,
             git_lab_client=git_lab_client,
             parent_group=child_group,
+            query_parameters=query_parameters,
         )
     return all_groups
 
@@ -43,6 +46,7 @@ def recurse_groups(
 def git_lab_groups_api(
         request: HttpRequest,
 ) -> JsonResponse | HttpResponse:
+    query_parameters: GitLabApiCommonQueryParameters = get_common_query_parameters(request=request)
     git_lab_client: Gitlab | None = get_git_lab_client()
     if git_lab_client is None:
         return generic_500(request=request)
@@ -56,20 +60,22 @@ def git_lab_groups_api(
         all_groups={group_parent},
         git_lab_client=git_lab_client,
         parent_group=group_parent,
+        query_parameters=query_parameters,
     )
     group_dicts: list[GitLabGroupTypedDict] = [group.asdict() for group in list(all_groups)]
     for group_dict in group_dicts:
         group_id: int | None = group_dict.get("id")
         if group_id is None:
             continue
-        git_lab_group: GitLabGroup = GitLabGroup.objects.get_or_create(id=group_id)[0]
+        get_or_create_tuple: tuple[GitLabGroup, bool] = GitLabGroup.objects.get_or_create(id=group_id)
+        git_lab_group: GitLabGroup = get_or_create_tuple[0]
         git_lab_group.avatar_url = group_dict.get("avatar_url")
-        git_lab_group.created_at = convert_and_enforce_utc_timezone(datetime_string=group_dict.get("created_at"))
         git_lab_group.description = group_dict.get("description")
         git_lab_group.full_name = group_dict.get("full_name")
         git_lab_group.full_path = group_dict.get("full_path")
         git_lab_group.name = group_dict.get("name")
         git_lab_group.path = group_dict.get("path")
         git_lab_group.web_url = group_dict.get("web_url")
+        git_lab_group.created_at = convert_and_enforce_utc_timezone(datetime_string=group_dict.get("created_at"))
         git_lab_group.save()
     return JsonResponse(data=group_dicts, safe=False)
--- git_lab/api/git_lab_users_api.py
+++ git_lab/api/git_lab_users_api.py
@@ -2,21 +2,22 @@ from typing import cast
 
 from django.db.models import QuerySet
 from django.http import HttpRequest, JsonResponse, HttpResponse
-from gitlab import Gitlab
+from gitlab import Gitlab, GitlabListError
 from gitlab.v4.objects import GroupMember, Group
 
 from core.utilities.cast_query_set import cast_query_set
 from core.utilities.convert_and_enforce_utc_timezone import convert_and_enforce_utc_timezone
 from core.utilities.git_lab.get_git_lab_client import get_git_lab_client
 from core.views.generic.generic_500 import generic_500
+from git_lab.api.common.get_common_query_parameters import GitLabApiCommonQueryParameters, get_common_query_parameters
 from git_lab.models.common.typed_dicts.git_lab_user_typed_dict import GitLabUserTypedDict
 from git_lab.models.git_lab_group import GitLabGroup
 from git_lab.models.git_lab_user import GitLabUser
 
-
 def git_lab_users_api(
         request: HttpRequest,
 ) -> JsonResponse | HttpResponse:
+    query_parameters: GitLabApiCommonQueryParameters = get_common_query_parameters(request=request)
     git_lab_client: Gitlab | None = get_git_lab_client()
     if git_lab_client is None:
         return generic_500(request=request)
@@ -26,25 +27,32 @@ def git_lab_users_api(
     )
     all_members: set[GroupMember] = set()
     for git_lab_group in git_lab_groups:
-        group: Group | None = git_lab_client.groups.get(id=git_lab_group.id)
-        if group is None:
+        try:
+            group: Group | None = git_lab_client.groups.get(id=git_lab_group.id)
+            if group is None:
+                continue
+            members: list[GroupMember] = cast(
+                typ=list[GroupMember],
+                val=group.members.list(**query_parameters)
+            )
+        except GitlabListError as error:
+            print(f"GitLabListError on {git_lab_group.full_path}: {error.error_message}")
             continue
-        members: list[GroupMember] = cast(typ=list[GroupMember], val=group.members.list(all=True))
-        for member in members:
-            all_members.add(member)
+        all_members.update(members)
     member_dicts: list[GitLabUserTypedDict] = [project.asdict() for project in list(all_members)]
     for member_dict in member_dicts:
         member_id: int | None = member_dict.get("id")
         if member_id is None:
             continue
-        git_lab_user: GitLabUser = GitLabUser.objects.get_or_create(id=member_id)[0]
+        get_or_create_tuple: tuple[GitLabUser, bool] = GitLabUser.objects.get_or_create(id=member_id)
+        git_lab_user: GitLabUser = get_or_create_tuple[0]
         git_lab_user.avatar_url = member_dict.get("avatar_url")
-        git_lab_user.created_at = convert_and_enforce_utc_timezone(datetime_string=member_dict.get("created_at"))
-        git_lab_user.expires_at = convert_and_enforce_utc_timezone(datetime_string=member_dict.get("expires_at"))
-        git_lab_user.locked = member_dict.get("locked")
-        git_lab_user.state = member_dict.get("membership_state")
+        git_lab_user.locked = member_dict.get("locked") or False
         git_lab_user.name = member_dict.get("name")
+        git_lab_user.state = member_dict.get("membership_state")
         git_lab_user.username = member_dict.get("username")
         git_lab_user.web_url = member_dict.get("web_url")
+        git_lab_user.created_at = convert_and_enforce_utc_timezone(datetime_string=member_dict.get("created_at"))
+        git_lab_user.expires_at = convert_and_enforce_utc_timezone(datetime_string=member_dict.get("expires_at"))
         git_lab_user.save()
     return JsonResponse(data=member_dicts, safe=False)
