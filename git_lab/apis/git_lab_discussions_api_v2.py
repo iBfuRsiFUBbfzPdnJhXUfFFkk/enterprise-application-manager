2cdcab34666aa1a370d0a8155cee1a010165f37a
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-13 13:38:29 -0600

    Add git_lab_discussions_api_v2 and update API endpoint
    Introduced a new version of the discussions API (v2) with updated handling of GitLab projects, merge requests, discussions, and notes within the last month. Updated the URL route for the discussions API to use the new implementation. This change ensures better functionality and alignment with recent requirements.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

---
+++ git_lab/apis/git_lab_discussions_api_v2.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from typing import cast
+
+from dateutil.relativedelta import relativedelta
+from django.db.models import QuerySet, Q
+from django.http import HttpRequest, JsonResponse, HttpResponse
+from gitlab import Gitlab
+from gitlab.base import RESTObjectList
+from gitlab.v4.objects import ProjectMergeRequest, Project
+
+from core.settings.common.developer import DEBUG
+from core.utilities.cast_query_set import cast_query_set
+from core.utilities.git_lab.get_git_lab_client import get_git_lab_client
+from core.views.generic.generic_500 import generic_500
+from git_lab.models.common.typed_dicts.git_lab_discussion_typed_dict import GitLabDiscussionTypedDict
+from git_lab.models.git_lab_project import GitLabProject
+
+
+def git_lab_discussions_api_v2(
+        request: HttpRequest,
+) -> JsonResponse | HttpResponse:
+    now: datetime = datetime.now()
+    one_month_ago: datetime = now - relativedelta(months=1)
+    git_lab_client: Gitlab | None = get_git_lab_client()
+    if git_lab_client is None:
+        return generic_500(request=request)
+    git_lab_projects: QuerySet[GitLabProject] = cast_query_set(
+        typ=GitLabProject,
+        val=GitLabProject.objects.filter(~Q(should_skip=True))
+    )
+    for git_lab_project in iter(git_lab_projects.all()):
+        if DEBUG is True:
+            print(f"Processing PROJECT: {git_lab_project.full_path}")
+        project_id: int = git_lab_project.id
+        project: Project | None = git_lab_client.projects.get(id=project_id, lazy=False)
+        if project is None:
+            continue
+        project_merge_request_generator: RESTObjectList = project.mergerequests.list(
+            iterator=True,
+            state="all",
+            order_by="created_at",
+            sort="desc",
+            created_after=one_month_ago,
+        )
+        for project_merge_request in project_merge_request_generator:
+            project_merge_request: ProjectMergeRequest = cast(
+                typ=ProjectMergeRequest,
+                val=project_merge_request,
+            )
+            if DEBUG is True:
+                print(f"----Processing MERGE REQUEST: {project_merge_request.web_url}")
+            discussion_generator: RESTObjectList = project_merge_request.discussions.list(
+                iterator=True,
+            )
+            for discussion in discussion_generator:
+                discussion_dict: GitLabDiscussionTypedDict = discussion.asdict()
+                if DEBUG is True:
+                    print(f"--------Processing DISCUSSION: {discussion_dict.get("id")}")
+                for note in iter(discussion_dict.get("notes")):
+                    if DEBUG is True:
+                        print(f"------------Processing NOTE: {note.get("id")}")
+    return JsonResponse(
+        data={},
+        safe=False
+    )
--- git_lab/urls/urlpatterns_git_lab_api.py
+++ git_lab/urls/urlpatterns_git_lab_api.py
@@ -2,6 +2,7 @@ from django.urls import URLPattern, URLResolver, path
 
 from git_lab.apis.git_lab_changes_api import git_lab_changes_api
 from git_lab.apis.git_lab_discussions_api import git_lab_discussions_api
+from git_lab.apis.git_lab_discussions_api_v2 import git_lab_discussions_api_v2
 from git_lab.apis.git_lab_groups_api import git_lab_groups_api
 from git_lab.apis.git_lab_issues_api import git_lab_issues_api
 from git_lab.apis.git_lab_iterations_api import git_lab_iterations_api
@@ -11,7 +12,7 @@ from git_lab.apis.git_lab_users_api import git_lab_users_api
 
 urlpatterns_git_lab_api: list[URLPattern | URLResolver] = [
     path(name='git_lab_api_changes', route='change/', view=git_lab_changes_api),
-    path(name='git_lab_api_discussions', route='discussion/', view=git_lab_discussions_api),
+    path(name='git_lab_api_discussions', route='discussion/', view=git_lab_discussions_api_v2),
     path(name='git_lab_api_groups', route='group/', view=git_lab_groups_api),
     path(name='git_lab_api_issues', route='issue/', view=git_lab_issues_api),
     path(name='git_lab_api_iterations', route='iteration/', view=git_lab_iterations_api),
