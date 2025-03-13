232468632ceb25d19dbfb3b5c2facb97547257cc
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-03-13 13:52:04 -0600

    Refactor GitLab API discussion processing logic
    Rename variables and update type annotations for clarity. Add checks for discussion notes to handle null cases and exclude system notes. Streamline debug print statements to improve readability during debugging.
    
    Signed-off-by: iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>

--- git_lab/apis/git_lab_discussions_api_v2.py
+++ git_lab/apis/git_lab_discussions_api_v2.py
@@ -14,6 +14,7 @@ from core.utilities.git_lab.get_git_lab_client import get_git_lab_client
 from core.views.generic.generic_500 import generic_500
 from git_lab.models.common.typed_dicts.git_lab_discussion_typed_dict import GitLabDiscussionTypedDict
 from git_lab.models.common.typed_dicts.git_lab_merge_request_typed_dict import GitLabMergeRequestTypedDict
+from git_lab.models.common.typed_dicts.git_lab_note_typed_dict import GitLabNoteTypedDict
 from git_lab.models.common.typed_dicts.git_lab_project_typed_dict import GitLabProjectTypedDict
 from git_lab.models.git_lab_project import GitLabProject
 
@@ -26,42 +27,48 @@ def git_lab_discussions_api_v2(
     git_lab_client: Gitlab | None = get_git_lab_client()
     if git_lab_client is None:
         return generic_500(request=request)
-    git_lab_projects: QuerySet[GitLabProject] = cast_query_set(
+    project_models: QuerySet[GitLabProject] = cast_query_set(
         typ=GitLabProject,
         val=GitLabProject.objects.filter(~Q(should_skip=True))
     )
-    for git_lab_project in iter(git_lab_projects.all()):
+    for project_model in iter(project_models.all()):
         if DEBUG is True:
-            print(f"Processing PROJECT: {git_lab_project.web_url}")
-        project_id: int = git_lab_project.id
-        project: Project | None = git_lab_client.projects.get(id=project_id, lazy=False)
-        if project is None:
+            print(f"Processing PROJECT: {project_model.web_url}")
+        project_id: int = project_model.id
+        project_rest_object: Project | None = git_lab_client.projects.get(id=project_id, lazy=False)
+        if project_rest_object is None:
             continue
-        project_merge_request_generator: RESTObjectList = project.mergerequests.list(
+        project_merge_request_generator: RESTObjectList = project_rest_object.mergerequests.list(
             iterator=True,
             state="all",
             order_by="created_at",
             sort="desc",
             created_after=one_month_ago,
         )
-        for project_merge_request in project_merge_request_generator:
-            project_merge_request: ProjectMergeRequest = cast(
-                typ=ProjectMergeRequest,
-                val=project_merge_request,
-            )
-            merge_request_dict: GitLabMergeRequestTypedDict = project_merge_request.asdict()
+        for project_merge_request_rest_object in project_merge_request_generator:
+            merge_request_dict: GitLabMergeRequestTypedDict = project_merge_request_rest_object.asdict()
             if DEBUG is True:
-                print(f"----Processing MERGE REQUEST: {merge_request_dict.get('web_url')}")
-            discussion_generator: RESTObjectList = project_merge_request.discussions.list(
+                print(f"----M: {merge_request_dict.get('web_url')}")
+            discussion_generator: RESTObjectList = project_merge_request_rest_object.discussions.list(
                 iterator=True,
             )
             for discussion in discussion_generator:
                 discussion_dict: GitLabDiscussionTypedDict = discussion.asdict()
                 if DEBUG is True:
-                    print(f"--------Processing DISCUSSION: {discussion_dict.get("id")}")
-                for note in iter(discussion_dict.get("notes")):
+                    print(f"--------D: {discussion_dict.get("id")}")
+                note_dicts: list[GitLabNoteTypedDict] | None = discussion_dict.get("notes")
+                if note_dicts is None:
+                    continue
+                for note_dict in iter(note_dicts):
+                    note: GitLabNoteTypedDict = note_dict
+                    is_system_note: bool | None = note.get("system")
+                    if is_system_note:
+                        continue
                     if DEBUG is True:
-                        print(f"------------Processing NOTE: {note.get("id")}")
+                        print(f"------------N: {note.get("id")}")
+                        print(f"------------N: {note.get("author").get('name')}")
+                        print(f"------------N: {note.get("author").get('username')}")
+                        print(f"------------N: {note.get("body")}")
     return JsonResponse(
         data={},
         safe=False
