158045a2deaaabb2988696ece44e1004077876b5
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2025-12-10 12:42:43 -0700

    feat: add date pickers to recommendation timeline fields
    Use HTML5 date input widgets for date_recommended, date_target_completion, and date_completed fields to provide native date picker functionality.

--- core/forms/recommendation_form.py
+++ core/forms/recommendation_form.py
@@ -1,3 +1,5 @@
+from django.forms import DateInput
+
 from core.forms.common.base_model_form import BaseModelForm
 from core.forms.common.base_model_form_meta import BaseModelFormMeta
 from core.models.recommendation import Recommendation
@@ -6,3 +8,8 @@ from core.models.recommendation import Recommendation
 class RecommendationForm(BaseModelForm):
     class Meta(BaseModelFormMeta):
         model = Recommendation
+        widgets = {
+            "date_recommended": DateInput(attrs={"type": "date"}),
+            "date_target_completion": DateInput(attrs={"type": "date"}),
+            "date_completed": DateInput(attrs={"type": "date"}),
+        }
