28e8e52425ab113df2e7e10466acae7bf5bd6533
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2026-01-28 12:19:23 -0700

    feat: add searchable chip input for document applications field
    - Replace checkbox multi-select with searchable dropdown and chips
    - Add CSS styles for chips, dropdown, and search input
    - Fix file input validation by using opacity overlay instead of hidden
    - Populate existing applications when editing documents
    - Handle reverse ManyToMany relation save for applications

--- core/forms/document_form.py
+++ core/forms/document_form.py
@@ -1,18 +1,45 @@
+from django.forms import ModelMultipleChoiceField, SelectMultiple
+
 from core.forms.common.base_model_form import BaseModelForm
 from core.forms.common.base_model_form_meta import BaseModelFormMeta
-from core.forms.common.generic_multiple_choice_field import generic_multiple_choice_field
 from core.models.application import Application
 from core.models.document import Document
 
 
 class DocumentForm(BaseModelForm):
-    applications = generic_multiple_choice_field(queryset=Application.objects.all())
+    applications = ModelMultipleChoiceField(
+        queryset=Application.objects.all().order_by('name'),
+        required=False,
+        widget=SelectMultiple(attrs={'class': 'searchable-multi-select'})
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Make file required only for new documents (not editing)
         if not self.instance.pk:
             self.fields['file'].required = True
+        else:
+            # Populate applications from reverse relation for existing documents
+            self.fields['applications'].initial = self.instance.applications.all()
+
+    def save(self, commit=True):
+        document = super().save(commit=commit)
+        if commit:
+            # Update the reverse ManyToMany relation
+            selected_apps = self.cleaned_data.get('applications', [])
+            # Get current applications that have this document
+            current_apps = set(document.applications.all())
+            selected_apps_set = set(selected_apps)
+
+            # Remove document from apps that are no longer selected
+            for app in current_apps - selected_apps_set:
+                app.documents.remove(document)
+
+            # Add document to newly selected apps
+            for app in selected_apps_set - current_apps:
+                app.documents.add(document)
+
+        return document
 
     class Meta(BaseModelFormMeta):
         model = Document
