9f0297d123874498246b3e4021c769d06d30958e
iBfuRsiFUBbfzPdnJhXUfFFkk <jowly.knowing_2x@icloud.com>
2026-01-28 12:13:57 -0700

    feat: add delete buttons to document list page
    - Create document_delete_view using generic delete pattern
    - Add delete URL route for documents
    - Add delete button to grid view cards
    - Add delete button to list view actions with confirmation dialog

--- core/urls/urlpatterns_document.py
+++ core/urls/urlpatterns_document.py
@@ -2,6 +2,7 @@ from django.urls import URLPattern, URLResolver, path
 
 from core.views.document.document_add_view import document_add_view
 from core.views.document.document_create_ajax_view import document_create_ajax_view
+from core.views.document.document_delete_view import document_delete_view
 from core.views.document.document_detail_view import document_detail_view
 from core.views.document.document_duplicates_view import document_duplicates_view
 from core.views.document.document_edit_view import document_edit_view
@@ -15,6 +16,7 @@ from core.views.document.document_view import document_view
 urlpatterns_document: list[URLPattern | URLResolver] = [
     path("document/", document_view, name="document"),
     path("document/edit/<int:model_id>/", document_edit_view, name="document_edit"),
+    path("document/delete/<int:model_id>/", document_delete_view, name="document_delete"),
     path("document/new/", document_add_view, name="document_new"),
     path("document/detail/<int:model_id>/", document_detail_view, name="document_detail"),
     path("document/create/ajax/", document_create_ajax_view, name="document_create_ajax"),
